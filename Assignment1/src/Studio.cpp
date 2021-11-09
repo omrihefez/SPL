7fa4e8e99456c01fcfc10187058c448011f65502
Omri Hefez <omrihe@gmail.com>
2021-11-09 16:43:29 +0200

    constructor of Studio class

--- Assignment1/src/Studio.cpp
+++ Assignment1/src/Studio.cpp
@@ -3,6 +3,10 @@
 //
 
 using namespace std;
+
+#include <iostream>
+#include <string>
+#include <fstream>
 #include "../include/Studio.h"
 
 
@@ -10,10 +14,75 @@ Studio::Studio() {
 
 }
 
-Studio::Studio(const std::string &configFilePath) {
+void buildWorkout(int id, string _line) {
+    string workoutName;
+    string workoutType;
+    WorkoutType type;
+    string priceString;
+    int price;
+    int index;
+    for (index = 0; _line[index] != ','; index++)
+        workoutName += _line[index];
+    index += 2;
+    for (index; _line[index] != ','; index++)
+        workoutType += _line[index];
+    if (workoutType[0] == 'A')
+        type = ANAEROBIC;
+    else if (workoutType[0] == 'C')
+        type = CARDIO;
+    else
+        type = MIXED;
+    index += 2;
+    for (index; index < _line.length(); index++)
+        priceString += _line[index];
+    price = stoi(priceString);
+    Workout(id, workoutName, price, type);
+}
 
+Studio::Studio(const std::string &configFilePath) {
+    string line;
+    fstream fstream1(configFilePath);
+    int lineNumber = 0;
+    while (getline(fstream1, line)) {
+        if (line[0] == '#'){
+            (getline(fstream1, line));
+            switch (lineNumber) {
+                case 0:
+                    string _numOfTrainers = "";
+                    for (int i = 0; i < line.length(); i++)
+                        _numOfTrainers += line[i];
+                    numOfTrainers = stoi(_numOfTrainers);
+                    lineNumber++;
+                    break;
+                case 1:
+                    string capacity = "";
+                    for (int i = 0; i < line.length(); i++) {
+                        if (line[i] != ',' & line[i] != ' ')
+                            capacity += line[i];
+                        else if (line[i] == ',') {
+                            Trainer(stoi(capacity));
+                            capacity = "";
+                        }
+
+
+                    }
+                    lineNumber++;
+                    break;
+                case 2:
+                    int id = 0;
+                    buildWorkout(id, line);
+                    id++;
+                    while (getline(fstream1, line)) {
+                        buildWorkout(id, line);
+                        id++;
+                    }
+                    break;
+            }
+        }
+    }
 
 }
+
 
 void Studio::start() {
     open = true;
@@ -21,7 +90,6 @@ void Studio::start() {
 
     }
 
-}
 
 int Studio::getNumOfTrainers() const {
     return 0;
@@ -38,3 +106,4 @@ const std::vector<BaseAction *> &Studio::getActionsLog() const {
 std::vector<Workout> &Studio::getWorkoutOptions() {
     return <#initializer#>;
 }
+
