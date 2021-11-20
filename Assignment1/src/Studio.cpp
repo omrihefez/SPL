bcbda9ec6b3d26578f9f9c6f2e54cfa8c4c52ee5
maorzam <maorzamsky@gmail.com>
2021-11-20 19:13:44 +0200

    StudioUpdate

--- Assignment1/src/Studio.cpp
+++ Assignment1/src/Studio.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 
 Studio::Studio() {
-
+int customersIdCounter = -1;
 }
 
 void buildWorkout(int id, string _line) {
@@ -40,6 +40,7 @@ void buildWorkout(int id, string _line) {
 }
 
 Studio::Studio(const std::string &configFilePath) {
+    open = false;
     string line;
     fstream fstream1(configFilePath);
     int lineNumber = 0;
@@ -63,8 +64,6 @@ Studio::Studio(const std::string &configFilePath) {
                             Trainer(stoi(capacity));
                             capacity = "";
                         }
-
-
                     }
                     lineNumber++;
                     break;
@@ -87,23 +86,102 @@ Studio::Studio(const std::string &configFilePath) {
 void Studio::start() {
     open = true;
     cout << "Studio is now open!" << endl;
+    string s;
+    cin << s;
+    switch (s) {
+        case s.substr(0,s.find_first_of(" ")-1) == "open":
+            int firstSpace= find_first_of(" ");
+            vector<Customer *> &customers;
+            int trainerId= stoi(s.substr[firstSpace+1,(s.substr(firstSpace+1,s.length-1)).find_first_of(" ")-1]);
+            int start = firstspace + 3;
+            while (start < s.length()) {
+                string name = "";
+                string customerIdString = "";
+                for (int i = start; i < (s.substr(start,s.length-1)).find_first_of(","); i++) {
+                    name += s[i];
+                    start = i;
+                }
+                for (int i = start+1 ; i < (s.substr(start,s.length-1)).find_first_of(" "); i++){
+                    customerIdString += s[i];
+                    start = i
+                }
+                Customer toAdd =  Customer(name,stoi(customerIdString));
+                customers.push_back(toAdd);
+                start ++;
+                start ++;
+           }
+            BaseAction a = OpenTrainer(trainerId,customers);
+            a.act(this);
+        case s.substr(0,s.find_first_of(" ")-1) == "order":
+            string trainerId = "";
+            for (int i = s.find_first_of(" ")+1 ; i < s.length() ; i++ )
+                trainerId += s[i];
+            BaseAction a = order(stoi(trainerId));
+            a.act(this);
+        case s.substr(0,s.find_first_of(" ")-1) == "move":
+            string src = "";
+            string dst = "";
+            string customerId = "";
+            start = s.find_first_of(" ")+1
+            for (int i = start; i < s.length() && s[i] != " "; i++) {
+                src += s[i];
+                start++;
+            }
+            for (int i = start++ ; i < s.length() && s[i] != " " ; i++){
+                dst += s[i];
+                start++;
+            }
+            for (int i= start++ ; i < s.length() ; i++){
+                customerId += s[i];
+            }
+            BaseAction a = MoveCustomer(stoi(src), stoi(dst), stoi(customerId));
+            a.act(this);
+        case s.substr(0,s.find_first_of(" ")-1) == "close":
+            int trainerId = stoi(s.substring(6,s.length()-1));
+            BaseAction a = close(trainerId);
+            a.act(this);
+        case s.substr(0,s.find_first_of(" ")-1) == "status":
+            int trainerId = stoi(s.substring(7,s.length()-1));
+            BaseAction a = PrintTrainerStatus(trainerId);
+            a.act(this);
+        case s == "workout_options":
+            BaseAction a = PrintWorkoutOptions();
+            a.act(this);
+        case s == "log":
+            BaseAction a = PrintActionsLog();
+            a.act(this);
+        case s == "backup":
+            BaseAction a = BackupStudio();
+            a.act(this);
+        case s == "restore":
+            BaseAction a = RestoreStudio();
+            a.act(this);
+        case s == "closeall":
+            BaseAction a = CloseAll();
+            a.act(this);
+            open = false;
+            break;
 
     }
+}
 
 
 int Studio::getNumOfTrainers() const {
-    return 0;
+    return trainers.size();
 }
 
 Trainer *Studio::getTrainer(int tid) {
-    return nullptr;
+    if (tid > trainers.size())
+        return nullptr;
+    else
+        return trainers[tid];
 }
 
 const std::vector<BaseAction *> &Studio::getActionsLog() const {
-    return <#initializer#>;
+    return actionsLog;
 }
 
 std::vector<Workout> &Studio::getWorkoutOptions() {
-    return <#initializer#>;
+    return workout_options;
 }
 
